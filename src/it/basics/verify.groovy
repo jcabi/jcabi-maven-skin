ace46bf532d8f0aae02c7242a89bdf85c6b46f79
amihaiemil <amihaiemil@gmail.com>
2016-02-12 19:36:51 +0200

    removed <tt> from the tests, updated todo and updated .gitignore

--- src/it/basics/verify.groovy
+++ src/it/basics/verify.groovy
@@ -64,22 +64,17 @@ MatcherAssert.assertThat(
 def htmlResponse = new ValidatorBuilder().html().validate(html)
 MatcherAssert.assertThat(
     htmlResponse.errors(),
-    /**
-     * @todo #86 This validation doesn't work because maven-site-plugin produces
-     *  invalid HTML5 output (still using TT element, which is obsolete in
-     *  HTML5). We're expecting exactly one error here, because of that.
-     */
-    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(1))
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(0))
 )
 MatcherAssert.assertThat(
     htmlResponse.warnings(),
     /**
      * @todo #86 This validation doesn't work because maven-site-plugin produces
-     *  invalid HTML5 output (it is using an obsolete NAME attribute on
-     *  some HTML elements). We're expecting exactly one warning here,
+     *  invalid HTML5 output (it is using obsolete attributes on
+     *  some HTML elements). We're expecting exactly six warnings here,
      *  because of that.
      */
-    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(3))
+    Matchers.describedAs(htmlResponse.toString(), Matchers.hasSize(6))
 )
 
 def cssResponse = new ValidatorBuilder().css().validate(
